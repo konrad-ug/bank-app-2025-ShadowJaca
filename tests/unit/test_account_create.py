11549e0f70af3e9feaaf0e661374ed64a82fda5f
Jakub <j.pokropski.434@studms.ug.edu.pl>
2025-10-15 15:55:17 +0200

    feat: promo codes tests added

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -20,3 +20,15 @@ class TestAccount:
     def test_pesel_empty(self):
         account = Account("John", "Doe", "")
         assert account.pesel == "Invalid"
+
+    def test_no_valid_promo_code(self):
+        account = Account("John", "Doe", "98309201942", "PROM_AB1")
+        assert account.balance == 50
+
+    def test_no_invalid_promo_code_prefix(self):
+        account = Account("John", "Doe", "98309201942", "PROMO_AB1")
+        assert account.balance == 0
+
+    def test_no_valid_promo_code_suffix(self):
+        account = Account("John", "Doe", "98309201942", "PROM_ABg1")
+        assert account.balance == 50
